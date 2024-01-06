613e828665d66fb0f108dbdd3d1284e51be1ded9
Jessica Nelson <jessica.nelson60@gmail.com>
2024-01-06 08:40:30 -0700

    added contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,49 @@
+<section class="bg-gray-100 py-12">
+  <div class="container mx-auto">
+    <h2 class="text-3xl font-semibold text-center mb-6">Contact Us</h2>
+
+    <div class="flex flex-col md:flex-row justify-between items-center">
+      <!-- Contact Form -->
+      <div class="w-full md:w-1/2">
+        <form action="#" method="post" class="max-w-md mx-auto">
+          <div class="mb-4">
+            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+            <input type="text" id="name" name="name" placeholder="Your Name" class="mt-1 p-2 w-full border rounded-md">
+          </div>
+
+          <div class="mb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" name="email" placeholder="Your Email" class="mt-1 p-2 w-full border rounded-md">
+          </div>
+
+          <div class="mb-4">
+            <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+            <textarea id="message" name="message" rows="4" placeholder="Your Message" class="mt-1 p-2 w-full border rounded-md"></textarea>
+          </div>
+
+          <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Send Message</button>
+        </form>
+      </div>
+
+      <!-- Contact Information -->
+      <div class="w-full md:w-1/2 mt-8 md:mt-0">
+        <div class="flex flex-col items-center md:items-start">
+          <p class="text-gray-700 mb-2">Feel free to reach out to us using the information below:</p>
+
+          <p class="font-semibold">Lash Lux</p>
+          <p class="text-gray-700">123 Lash Street</p>
+          <p class="text-gray-700">City, Province, Postal Code</p>
+          
+          <p class="font-semibold mt-4">Contact Information:</p>
+          <p class="text-gray-700">Phone: (555) 123-4567</p>
+          <p class="text-gray-700">Email: info@example.com</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <div class="mt-8 flex justify-center">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.0270709590127!2d-117.9230550742938!3d33.811614473248504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcd7d12b3b5e6b%3A0x2ef62f8418225cfa!2sDisneyland%20Park!5e0!3m2!1sen!2sca!4v1704555533600!5m2!1sen!2sca" width="450" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</section>
